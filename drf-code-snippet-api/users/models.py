accccf9bd1b832f8c30f829a387092f1f42be068
Eric Ndivo <ericndivo@sallys-MacBook-Air.local>
2025-01-16 14:38:45 +0300

    snippets/comments

--- drf-code-snippet-api/users/models.py
+++ drf-code-snippet-api/users/models.py
@@ -15,3 +15,25 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+class Snippet(models.Model):
+    title = models.CharField(max_length=100)
+    code = models.TextField()
+    language = models.CharField(max_length=50)
+    version = models.CharField(max_length=50)
+    tags = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+class comment(models.Model):
+    snippet = models.ForeignKey(Snippet, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'comment by {self.author.username} on {self.snippet.title}'
